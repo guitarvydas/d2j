6ed3e1e1be81f2b93d0e1d89ca65eb73492712ec
Paul Tarvydas <guitarvydas@gmail.com>
2021-11-04 17:07:01 -0400

    wip: tabs don't work yet

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-d2j
+diagram to JSON transpiler
+
+input: draw.io diagram containing tabs containing diagrams
+
+diagram contains only:
+- rectangles
+- ellipses
+- text (value)
+- edges
+
+
+output: JSON array of diagrams (1 per drawio tab)
+
+status: tabs don't work yet, allow only 1 tab with 1 diagram on it
--- fb.pl
+++ fb.pl
@@ -1,6 +1,16 @@
 
 cell(0,"").
 cell(1,"").
+cell(6erVy3Hk52H6__b5spFqv__0,"").
+cell(6erVy3Hk52H6__b5spFqv__1,"").
+cell(6erVy3Hk52H6__b5spFqv__2,"").
+cell(6erVy3Hk52H6__b5spFqv__3,"").
+cell(6erVy3Hk52H6__b5spFqv__4,"").
+cell(6erVy3Hk52H6__b5spFqv__5,"").
+cell(6erVy3Hk52H6__b5spFqv__6,"").
+cell(6erVy3Hk52H6__b5spFqv__7,"").
+cell(6erVy3Hk52H6__b5spFqv__8,"").
+cell(6erVy3Hk52H6__b5spFqv__9,"").
 cell(i5jZ3dy8F4nkPbaXPHF6__1,"").
 cell(i5jZ3dy8F4nkPbaXPHF6__10,"").
 cell(i5jZ3dy8F4nkPbaXPHF6__2,"").
@@ -10,6 +20,7 @@ cell(i5jZ3dy8F4nkPbaXPHF6__5,"").
 cell(i5jZ3dy8F4nkPbaXPHF6__6,"").
 cell(i5jZ3dy8F4nkPbaXPHF6__9,"").
 diagram(fo9xvGcHm88d3B_x5weG, "").
+diagram(mcpuCb2xR66ZOdV7i_3L, "").
 diagramContains(fo9xvGcHm88d3B_x5weG, 0).
 diagramContains(fo9xvGcHm88d3B_x5weG, 1).
 diagramContains(fo9xvGcHm88d3B_x5weG, i5jZ3dy8F4nkPbaXPHF6__1).
@@ -20,13 +31,37 @@ diagramContains(fo9xvGcHm88d3B_x5weG, i5jZ3dy8F4nkPbaXPHF6__4).
 diagramContains(fo9xvGcHm88d3B_x5weG, i5jZ3dy8F4nkPbaXPHF6__5).
 diagramContains(fo9xvGcHm88d3B_x5weG, i5jZ3dy8F4nkPbaXPHF6__6).
 diagramContains(fo9xvGcHm88d3B_x5weG, i5jZ3dy8F4nkPbaXPHF6__9).
+diagramContains(mcpuCb2xR66ZOdV7i_3L, 6erVy3Hk52H6__b5spFqv__0).
+diagramContains(mcpuCb2xR66ZOdV7i_3L, 6erVy3Hk52H6__b5spFqv__1).
+diagramContains(mcpuCb2xR66ZOdV7i_3L, 6erVy3Hk52H6__b5spFqv__2).
+diagramContains(mcpuCb2xR66ZOdV7i_3L, 6erVy3Hk52H6__b5spFqv__3).
+diagramContains(mcpuCb2xR66ZOdV7i_3L, 6erVy3Hk52H6__b5spFqv__4).
+diagramContains(mcpuCb2xR66ZOdV7i_3L, 6erVy3Hk52H6__b5spFqv__5).
+diagramContains(mcpuCb2xR66ZOdV7i_3L, 6erVy3Hk52H6__b5spFqv__6).
+diagramContains(mcpuCb2xR66ZOdV7i_3L, 6erVy3Hk52H6__b5spFqv__7).
+diagramContains(mcpuCb2xR66ZOdV7i_3L, 6erVy3Hk52H6__b5spFqv__8).
+diagramContains(mcpuCb2xR66ZOdV7i_3L, 6erVy3Hk52H6__b5spFqv__9).
+edge(6erVy3Hk52H6__b5spFqv__5, "").
 edge(i5jZ3dy8F4nkPbaXPHF6__4, "").
+ellipse(6erVy3Hk52H6__b5spFqv__6, "").
+ellipse(6erVy3Hk52H6__b5spFqv__8, "").
 ellipse(i5jZ3dy8F4nkPbaXPHF6__5, "").
 ellipse(i5jZ3dy8F4nkPbaXPHF6__9, "").
+fillColor(6erVy3Hk52H6__b5spFqv__4, "red").
+fillColor(6erVy3Hk52H6__b5spFqv__6, "yellow").
+fillColor(6erVy3Hk52H6__b5spFqv__8, "green").
+fillColor(6erVy3Hk52H6__b5spFqv__9, "red").
 fillColor(i5jZ3dy8F4nkPbaXPHF6__10, "red").
 fillColor(i5jZ3dy8F4nkPbaXPHF6__3, "red").
 fillColor(i5jZ3dy8F4nkPbaXPHF6__5, "yellow").
 fillColor(i5jZ3dy8F4nkPbaXPHF6__9, "green").
+height(6erVy3Hk52H6__b5spFqv__2, 480).
+height(6erVy3Hk52H6__b5spFqv__3, 170).
+height(6erVy3Hk52H6__b5spFqv__4, 80).
+height(6erVy3Hk52H6__b5spFqv__6, 40).
+height(6erVy3Hk52H6__b5spFqv__7, 150).
+height(6erVy3Hk52H6__b5spFqv__8, 40).
+height(6erVy3Hk52H6__b5spFqv__9, 75).
 height(i5jZ3dy8F4nkPbaXPHF6__1, 480).
 height(i5jZ3dy8F4nkPbaXPHF6__10, 75).
 height(i5jZ3dy8F4nkPbaXPHF6__2, 170).
@@ -34,7 +69,8 @@ height(i5jZ3dy8F4nkPbaXPHF6__3, 80).
 height(i5jZ3dy8F4nkPbaXPHF6__5, 40).
 height(i5jZ3dy8F4nkPbaXPHF6__6, 150).
 height(i5jZ3dy8F4nkPbaXPHF6__9, 40).
-name(fo9xvGcHm88d3B_x5weG, "Page-1").
+name(fo9xvGcHm88d3B_x5weG, "Hello World").
+name(yyy, "Goodbye World").
 rect(0,"").
 rect(1,"").
 rect(i5jZ3dy8F4nkPbaXPHF6__1,"").
@@ -42,8 +78,17 @@ rect(i5jZ3dy8F4nkPbaXPHF6__10,"").
 rect(i5jZ3dy8F4nkPbaXPHF6__2,"").
 rect(i5jZ3dy8F4nkPbaXPHF6__3,"").
 rect(i5jZ3dy8F4nkPbaXPHF6__6,"").
+source(6erVy3Hk52H6__b5spFqv__5, 6erVy3Hk52H6__b5spFqv__6).
 source(i5jZ3dy8F4nkPbaXPHF6__4, i5jZ3dy8F4nkPbaXPHF6__5).
+target(6erVy3Hk52H6__b5spFqv__5, 6erVy3Hk52H6__b5spFqv__8).
 target(i5jZ3dy8F4nkPbaXPHF6__4, i5jZ3dy8F4nkPbaXPHF6__9).
+value(6erVy3Hk52H6__b5spFqv__2, "helloworld").
+value(6erVy3Hk52H6__b5spFqv__3, "hello").
+value(6erVy3Hk52H6__b5spFqv__4, "&lt;div&gt;echo goodbye&lt;/div&gt;&lt;div&gt;&lt;/div&gt;echo done &amp;gt;/dev/fd/4").
+value(6erVy3Hk52H6__b5spFqv__6, "out").
+value(6erVy3Hk52H6__b5spFqv__7, "world").
+value(6erVy3Hk52H6__b5spFqv__8, "in").
+value(6erVy3Hk52H6__b5spFqv__9, "&lt;p class=&quot;p1&quot; style=&quot;margin: 0px ; font-stretch: normal ; font-size: 11px ; line-height: normal ; font-family: &amp;#34;menlo&amp;#34;&quot;&gt;&lt;span class=&quot;s1&quot;&gt;read &amp;lt;/dev/fd/3&lt;/span&gt;&lt;/p&gt;&lt;p class=&quot;p1&quot; style=&quot;margin: 0px ; font-stretch: normal ; font-size: 11px ; line-height: normal ; font-family: &amp;#34;menlo&amp;#34;&quot;&gt;&lt;span class=&quot;s1&quot;&gt;echo world&lt;/span&gt;&lt;/p&gt;&lt;p class=&quot;p1&quot; style=&quot;margin: 0px ; font-stretch: normal ; font-size: 11px ; line-height: normal ; font-family: &amp;#34;menlo&amp;#34;&quot;&gt;&lt;span class=&quot;s1&quot;&gt;# no output&lt;/span&gt;&lt;/p&gt;&lt;p class=&quot;p1&quot; style=&quot;margin: 0px ; font-stretch: normal ; font-size: 11px ; line-height: normal ; font-family: &amp;#34;menlo&amp;#34;&quot;&gt;&lt;br&gt;&lt;/p&gt;").
 value(i5jZ3dy8F4nkPbaXPHF6__1, "helloworld").
 value(i5jZ3dy8F4nkPbaXPHF6__10, "&lt;p class=&quot;p1&quot; style=&quot;margin: 0px ; font-stretch: normal ; font-size: 11px ; line-height: normal ; font-family: &amp;#34;menlo&amp;#34;&quot;&gt;&lt;span class=&quot;s1&quot;&gt;read &amp;lt;/dev/fd/3&lt;/span&gt;&lt;/p&gt;&lt;p class=&quot;p1&quot; style=&quot;margin: 0px ; font-stretch: normal ; font-size: 11px ; line-height: normal ; font-family: &amp;#34;menlo&amp;#34;&quot;&gt;&lt;span class=&quot;s1&quot;&gt;echo world&lt;/span&gt;&lt;/p&gt;&lt;p class=&quot;p1&quot; style=&quot;margin: 0px ; font-stretch: normal ; font-size: 11px ; line-height: normal ; font-family: &amp;#34;menlo&amp;#34;&quot;&gt;&lt;span class=&quot;s1&quot;&gt;# no output&lt;/span&gt;&lt;/p&gt;&lt;p class=&quot;p1&quot; style=&quot;margin: 0px ; font-stretch: normal ; font-size: 11px ; line-height: normal ; font-family: &amp;#34;menlo&amp;#34;&quot;&gt;&lt;br&gt;&lt;/p&gt;").
 value(i5jZ3dy8F4nkPbaXPHF6__2, "hello").
@@ -51,6 +96,13 @@ value(i5jZ3dy8F4nkPbaXPHF6__3, "&lt;div&gt;echo hello&lt;/div&gt;&lt;div&gt;&lt;
 value(i5jZ3dy8F4nkPbaXPHF6__5, "out").
 value(i5jZ3dy8F4nkPbaXPHF6__6, "world").
 value(i5jZ3dy8F4nkPbaXPHF6__9, "in").
+vertex(6erVy3Hk52H6__b5spFqv__2, "").
+vertex(6erVy3Hk52H6__b5spFqv__3, "").
+vertex(6erVy3Hk52H6__b5spFqv__4, "").
+vertex(6erVy3Hk52H6__b5spFqv__6, "").
+vertex(6erVy3Hk52H6__b5spFqv__7, "").
+vertex(6erVy3Hk52H6__b5spFqv__8, "").
+vertex(6erVy3Hk52H6__b5spFqv__9, "").
 vertex(i5jZ3dy8F4nkPbaXPHF6__1, "").
 vertex(i5jZ3dy8F4nkPbaXPHF6__10, "").
 vertex(i5jZ3dy8F4nkPbaXPHF6__2, "").
@@ -58,6 +110,13 @@ vertex(i5jZ3dy8F4nkPbaXPHF6__3, "").
 vertex(i5jZ3dy8F4nkPbaXPHF6__5, "").
 vertex(i5jZ3dy8F4nkPbaXPHF6__6, "").
 vertex(i5jZ3dy8F4nkPbaXPHF6__9, "").
+width(6erVy3Hk52H6__b5spFqv__2, 460).
+width(6erVy3Hk52H6__b5spFqv__3, 235).
+width(6erVy3Hk52H6__b5spFqv__4, 165).
+width(6erVy3Hk52H6__b5spFqv__6, 40).
+width(6erVy3Hk52H6__b5spFqv__7, 245).
+width(6erVy3Hk52H6__b5spFqv__8, 40).
+width(6erVy3Hk52H6__b5spFqv__9, 175).
 width(i5jZ3dy8F4nkPbaXPHF6__1, 460).
 width(i5jZ3dy8F4nkPbaXPHF6__10, 175).
 width(i5jZ3dy8F4nkPbaXPHF6__2, 235).
@@ -65,6 +124,13 @@ width(i5jZ3dy8F4nkPbaXPHF6__3, 165).
 width(i5jZ3dy8F4nkPbaXPHF6__5, 40).
 width(i5jZ3dy8F4nkPbaXPHF6__6, 245).
 width(i5jZ3dy8F4nkPbaXPHF6__9, 40).
+x(6erVy3Hk52H6__b5spFqv__2, 170).
+x(6erVy3Hk52H6__b5spFqv__3, 255).
+x(6erVy3Hk52H6__b5spFqv__4, 285).
+x(6erVy3Hk52H6__b5spFqv__6, 470).
+x(6erVy3Hk52H6__b5spFqv__7, 265).
+x(6erVy3Hk52H6__b5spFqv__8, 245).
+x(6erVy3Hk52H6__b5spFqv__9, 295).
 x(i5jZ3dy8F4nkPbaXPHF6__1, 170).
 x(i5jZ3dy8F4nkPbaXPHF6__10, 295).
 x(i5jZ3dy8F4nkPbaXPHF6__2, 255).
@@ -72,6 +138,13 @@ x(i5jZ3dy8F4nkPbaXPHF6__3, 285).
 x(i5jZ3dy8F4nkPbaXPHF6__5, 470).
 x(i5jZ3dy8F4nkPbaXPHF6__6, 265).
 x(i5jZ3dy8F4nkPbaXPHF6__9, 245).
+y(6erVy3Hk52H6__b5spFqv__2, 170).
+y(6erVy3Hk52H6__b5spFqv__3, 220).
+y(6erVy3Hk52H6__b5spFqv__4, 265).
+y(6erVy3Hk52H6__b5spFqv__6, 290).
+y(6erVy3Hk52H6__b5spFqv__7, 450).
+y(6erVy3Hk52H6__b5spFqv__8, 507.5).
+y(6erVy3Hk52H6__b5spFqv__9, 490).
 y(i5jZ3dy8F4nkPbaXPHF6__1, 170).
 y(i5jZ3dy8F4nkPbaXPHF6__10, 490).
 y(i5jZ3dy8F4nkPbaXPHF6__2, 220).
